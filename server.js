9659ffdfc1ddf1a11f5996fc239f08ed83b1a490
Akobir <salomxayir.sx@gmail.com>
2021-03-26 09:48:21 +0300

    remove to connect mongodb

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // if(process.env.NODE_ENV !== "product"){
 //    require("dotenv")
 // }
-const dotenv  = require("dotenv");
-dotenv.config()
+// const dotenv  = require("dotenv");
+// dotenv.config()
 
 const express = require("express")
 const app = express();
@@ -17,10 +17,10 @@ app.use(expresslayouts);
 app.use(express.static("public"));
 app.use("/",expressRouter);
 // app.settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
-console.log(process.env.Database_Url)
-mongoose.connect(process.env.Database_Url,{useNewUrlParser:true,useUnifiedTopology:true});
-const db = mongoose.connect
-console.log(db);
+//console.log(process.env.Database_Url)
+//mongoose.connect(process.env.Database_Url,{useNewUrlParser:true,useUnifiedTopology:true});
+// const db = mongoose.connect
+// console.log(db);
 // db.on("error",(error) => { console.error(error)});
 // db.once("open",() => {console.log("Connected to mongodb")})
 
