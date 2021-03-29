03c16aa73e116a2b5abb7d1cfa9e44db9fd9737a
Akobir <salomxayir.sx@gmail.com>
2021-03-29 16:03:34 +0300

    changes some things

--- server.js
+++ server.js
@@ -1,32 +1,29 @@
 // if(process.env.NODE_ENV !== "product"){
 //    const dotenv = require("dotenv")
 // }
-// const dotenv  = require("dotenv");
-// const watch = dotenv.config();
-// console.log(watch);
-// const express = require("express")
-// const app = express();
-// const expresslayouts = require("express-ejs-layouts");
-// const expressRouter = require("./router/index")
-// const mongoose = require("mongoose");
+const dotenv  = require("dotenv");
+ dotenv.config();
 
-// app.set("view engine", "ejs")
-// app.set("views", __dirname + "/views");
-// app.set("layout","layouts/layout");
-// app.use(expresslayouts);
-// app.use(express.static("public"));
-// app.use("/",expressRouter);
-// // app.settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
-// console.log(process.env.Database_Url)
-// const db = mongoose.createConnection(process.env.Database_Url,{useNewUrlParser:true,useUnifiedTopology:true});
+const express = require("express")
+const app = express();
+const expresslayouts = require("express-ejs-layouts");
+const expressRouter = require("./router/index")
+const mongoose = require("mongoose");
+
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/views");
+app.set("layout","layouts/layout");
+app.use(expresslayouts);
+app.use(express.static("public"));
+app.use("/",expressRouter);
+// app.settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+
+const db = mongoose.createConnection(process.env.Database_Url,{useNewUrlParser:true,useUnifiedTopology:true});
 
 
-// db.on("error",(error) => { console.error(error)});
-// db.once("open",() => {console.log("Connected to mongodb")})
+db.on("error",(error) => { console.error(error)});
+db.once("open",() => {console.log("Connected to mongodb")})
 
-// app.listen(process.env.Port || 3000,() => {
-//     console.log("server eshitilyapti")
-// })
-const a = 3;
-const b = 5;
-console.log(a+b);
+app.listen(process.env.PORT || 8080,() => {
+    console.log("server eshitilyapti")
+})
