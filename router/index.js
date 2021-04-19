f2ad0527ef062e87253befe682b7e3437b6f9a34
Akobir <salomxayir.sx@gmail.com>
2021-04-19 14:20:06 +0300

    secondly change

--- router/index.js
+++ router/index.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const router = express.Router();
-
-router.get("/", (req,res) => {
-       res.render('index')
+const Book = require("../models/books")
+router.get("/", async (req,res) => {
+       let books
+       try{
+        books = await Book.find().sort({createdAt:"desc"}).limit(5)
+       }catch{
+        books = []
+       }
+       res.render('index',{books:books})
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const expresslayouts = require("express-ejs-layouts");
 const expressRouter = require("./router/index")
 const authorsRoute = require("./router/author");
+const booksRoute = require("./router/books")
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser")
 
@@ -19,6 +20,7 @@ app.use(express.static("public"));
 app.use("/",expressRouter);
 app.use(bodyParser.urlencoded({limit:"10mb",extended:false}))
 app.use("/authors",authorsRoute);
+app.use("/books",booksRoute);
 // app.settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
 
 mongoose.connect(process.env.Database_Url,{useNewUrlParser:true,useUnifiedTopology:true})
